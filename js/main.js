4a8600caa7f8414e05cd82f19755f3df27d8e3bb
Cristhian <virutasoft@gmail.com>
2022-02-04 20:19:29 -0500

    Testimonials section finished

--- js/main.js
+++ js/main.js
@@ -31,3 +31,48 @@ modalClose.forEach((mc) => {
     });
 });
 // MODAL SERVICES
+
+// MIXITUP FILTER PORTFOLIO
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 600
+    }
+});
+// MIXITUP FILTER PORTFOLIO
+
+// active link work
+const linkWork = document.querySelectorAll('.work__item')
+function activeWork() {
+    linkWork.forEach(l=>l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener('click', activeWork))
+// active link work
+
+// swiper slide
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    spaceBetween: 24,
+    loop:true,
+    grabCursor:true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        576: {
+          slidesPerView: 2,
+
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 48,
+        },
+      },
+
+  });
+// swiper slide
