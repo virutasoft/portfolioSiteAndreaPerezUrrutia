6158240386d2a7eb7f62ebf0993b819f90395aca
Cristhian <virutasoft@gmail.com>
2022-02-07 20:29:11 -0500

    Scroll page semi finished

--- js/main.js
+++ js/main.js
@@ -76,3 +76,26 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
 
   });
 // swiper slide
+
+
+// SCROLL SECTION ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+// SCROLL SECTION ACTIVE LINK
+
